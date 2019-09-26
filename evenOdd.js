f3cbc32962d9f5c9f62edef3102f5418a7f5b642
bitwisebhm <murphydemn@gmail.com>
2019-09-26 09:36:36 +0100

    Add files via upload

---
+++ evenOdd.js
@@ -0,0 +1,13 @@
+function showNumber(limit){
+    
+    for(var i = 0; i <= limit; i++){
+    if(i % 2 === 0)
+      console.log(i, "Even");
+      
+    else
+      console.log(i, "Odd");
+}  
+}
+showNumber(10)
+
+
